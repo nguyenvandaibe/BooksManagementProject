49f80805e0bd1d68cfe62d664cfa6e7187ec0211
Nguyen Van Dai <nguyenvandaibe@gmail.com>
2021-04-10 18:49:16 +0700

    Viết các class, interface kết nối DB, BaseModel, User login. Lỗi: không nạp được com.mysql.cj.jdbc.Driver.

--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,5 +1,41 @@
 package models;
 
 public class Book {
+	private Integer id;
+	private String name;
+	private String publisher;
+	private Integer price;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
 
 }
--- src/main/java/servlets/LoginServlet.java
+++ src/main/java/servlets/LoginServlet.java
@@ -1,34 +1,44 @@
 package servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
+import javax.naming.NamingException;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import models.User;
+
 @SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
-	public LoginServlet() {
+	public LoginServlet() throws NamingException {
 		super();
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+
 		String username = req.getParameter("username");
 		String password = req.getParameter("password");
-		
+
 		log(username);
 		log(password);
-		
-		// truy van co so du lieu bang model user
-		// 
-		
-		// cau hinh cookie (lam sau)
-		//
-		
-//		RequestDispatcher view = req.getRequestDispatcher("search-form.jsp");
-//		view.forward(req, resp);
-		resp.sendRedirect("search-form.jsp");
-		
+
+		try {
+			// truy van co so du lieu bang model user
+			if ((new User()).login(username, password)) {
+				// cau hinh cookie (lam sau)
+
+				resp.sendRedirect("search-form.jsp");
+			} else {
+				log("Thong tin dang nhap khong dung");
+				String url = "/login.jsp";
+				RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(url);
+				requestDispatcher.forward(req, resp);
+			}
+		} catch (ClassNotFoundException | SQLException | NamingException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
